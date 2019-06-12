c886dc52e2bac1d2363268dc61cef69f7a758fcc
vtorres96 <victorcdc96@gmail.com>
2019-06-12 19:47:02 -0300

    consertando for

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function magic(){
     var imgd = runnerCanvasContext.getImageData(rCanvas.width/8, rCanvas.height - rCanvas.height/2.5, rCanvas.width/7, rCanvas.height/5) 
     var jumpFlag = false 
     var pix = imgd.data 
-    for (var i = 0, n = pix.length  i < n  i += 4) {
+    for (var i = 0, n = pix.length; i < n;  i += 4) {
      if(pix[i]>0||pix[i+1]>0||pix[i+2]>0){
       jumpFlag = true 
       break 
